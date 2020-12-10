8e256b5f0b758249d50744d68e6ad156c554dd79
DonPandos <ewrr_96@mail.ru>
2020-12-10 22:14:58 +0300

    First commit

---
+++ src/main/java/com/spring/securityjwttemplate/SpringSecurityJwtAuthTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.spring.securityjwttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtAuthTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtAuthTemplateApplication.class, args);
+	}
+
+}
