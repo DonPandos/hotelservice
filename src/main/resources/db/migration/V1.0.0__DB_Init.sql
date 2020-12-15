316b58e5974caf11849b3eb50a8a35a3c106a320
DonPandos <ewrr_96@mail.ru>
2020-12-15 13:40:26 +0300

    Few little changes

--- src/main/resources/db/migration/V1.0.0__DB_Init.sql
+++ src/main/resources/db/migration/V1.0.0__DB_Init.sql
@@ -1,25 +1,43 @@
-CREATE TABLE users (
-    id SERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE users
+(
+    id       SERIAL  NOT NULL PRIMARY KEY,
     username VARCHAR NOT NULL,
     password VARCHAR NOT NULL,
-    status VARCHAR NOT NULL,
-    role_id INTEGER NOT NULL
+    status   VARCHAR NOT NULL,
+    role_id  INTEGER NOT NULL
 );
 
-CREATE TABLE roles (
-    id SERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE roles
+(
+    id   SERIAL  NOT NULL PRIMARY KEY,
     role VARCHAR NOT NULL
 );
 
-CREATE TABLE permissions (
-    id SERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE permissions
+(
+    id         SERIAL  NOT NULL PRIMARY KEY,
     permission VARCHAR NOT NULL
 );
 
-CREATE TABLE role_permissions (
-    id SERIAL NOT NULL PRIMARY KEY,
-    role_id INTEGER NOT NULL,
+CREATE TABLE role_permissions
+(
+    id            SERIAL  NOT NULL PRIMARY KEY,
+    role_id       INTEGER NOT NULL,
     permission_id INTEGER NOT NULL,
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (permission_id) REFERENCES permissions(id)
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    FOREIGN KEY (permission_id) REFERENCES permissions (id)
 );
+
+CREATE TABLE countries
+(
+    id      SERIAL  NOT NULL PRIMARY KEY,
+    country VARCHAR NOT NULL
+);
+
+CREATE TABLE cities
+(
+    id         SERIAL  NOT NULL PRIMARY KEY,
+    city       VARCHAR NOT NULL,
+    country_id INTEGER NOT NULL,
+    FOREIGN KEY (country_id) REFERENCES countries (id)
+)
